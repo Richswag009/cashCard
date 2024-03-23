1ee64ce66715fdf17aaeab18e3367ec619062f9f
Richswag009 <richesmetelewawon@gmail.com>
2024-03-24 00:28:34 +0100

    added test  and created a get endpoint

--- notification/src/main/java/com/richcodes/registration/RegistrationController.java
+++ notification/src/main/java/com/richcodes/registration/RegistrationController.java
@@ -1,2 +1,26 @@
-package com.richcodes.registration;public class RegistrationController {
+package com.richcodes.registration;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+    private  final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+
+    @PostMapping
+    public  void register(@RequestBody RegistrationRequest request ){
+
+        registrationService.register(request);
+    }
 }
--- notification/src/main/java/com/richcodes/registration/RegistrationRequest.java
+++ notification/src/main/java/com/richcodes/registration/RegistrationRequest.java
@@ -1,2 +1,22 @@
-package com.richcodes.registration;public class RegistrationRequest {
+package com.richcodes.registration;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private  final String firstName;
+    private  final String lastName;
+    private  final String email;
+    private  final String password;
+
+    public RegistrationRequest(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
 }
