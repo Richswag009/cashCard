a15065f62af692deaa477d3b60de68338cb4ae64
Riches Onuwaje Metelewawon <richesmetelewawon@gmail.com>
2024-03-24 01:09:51 +0100

    Delete hotelManagementSystem directory

--- hotelManagementSystem/backend/src/main/java/com/richcodes/dto/HotelRequest.java
+++
@@ -1,4 +0,0 @@
-package com.richcodes.dto;
-
-public record HotelRequest(double price,Integer roomNumber,String roomType) {
-}
--- hotelManagementSystem/backend/src/main/java/com/richcodes/repository/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.richcodes.repository;
-
-import com.richcodes.dto.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Integer> {
-}
